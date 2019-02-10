914396d2d5c3d413ef9904249a6d663d5a765831
Yaf98 <38588601+Yaf98@users.noreply.github.com>
2019-02-10 01:09:52 -0600

    #Agregar js y Terminar Laboratorio 7

---
+++ Lab 7/jscript.js
@@ -0,0 +1,6 @@
+  M.AutoInit();
+
+ document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.materialboxed');
+    var instances = M.Materialbox.init(elems, options);
+  });
